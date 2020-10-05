4ab96d8eb86ccd512caa8254f89445d3905abfc8
Anton Bazhan <your@email>
2020-10-05 16:21:48 +0300

    feat: function towelSort

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function towelSort(matrix) {
-    return [];
+    if (!Array.isArray(matrix) || matrix <= 0) {
+        return [];
+    }
+    const concat = matrix.reduce(function (result, current, index) {
+        if (index % 2 !== 0) {
+            current.reverse();
+        }
+        return result.concat(current);
+    }, []);
+    return concat;
 };
